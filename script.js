3580105854465507aaad98fcfa588ba84979eddc
AhmedovIM <ahmedovilas4@gmail.com>
2025-02-14 15:39:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
     groups.forEach(group => {
         group.addEventListener("click", function () {
+            // Скрыть все подгруппы
+            groups.forEach(otherGroup => {
+                const subgroup = otherGroup.nextElementSibling;
+                if (otherGroup !== group) {
+                    subgroup.style.display = "none";
+                }
+            });
+
+            // Переключить видимость текущей подгруппы
             const subgroup = this.nextElementSibling;
             subgroup.style.display = subgroup.style.display === "block" ? "none" : "block";
         });
